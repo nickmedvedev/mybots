dc872261be565c28dd6eda6bbcbe8b79f0f38272
nickmedvedev <115434259+nickmedvedev@users.noreply.github.com>
2023-01-09 14:19:50 -0600

    Stepping the World

--- simulate.py
+++ simulate.py
@@ -1,2 +1,14 @@
 import pybullet as p
-pass
+import time
+
+physicsClient = p.connect(p.GUI)
+print("Hi")
+
+for i in range(1000):
+    p.stepSimulation()
+    time.sleep(1/30)
+    print(i)
+
+p.disconnect()
+
+    
