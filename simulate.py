22a0a5af59b32ddeecc9e1f67655bc17cfe3df1b
nickmedvedev <115434259+nickmedvedev@users.noreply.github.com>
2023-01-15 22:20:17 -0600

    Update simulate.py

--- simulate.py
+++ simulate.py
@@ -1,6 +1,8 @@
 import pybullet as p
 import time
 import pybullet_data
+import pyrosim.pyrosim as pyrosim
+
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -13,11 +15,15 @@ robotId = p.loadURDF("body.urdf")
 
 p.loadSDF("world.sdf")
 
+pyrosim.Prepare_To_Simulate(robotId)
 for i in range(1000):
     p.stepSimulation()
+    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+    print(backLegTouch)
     time.sleep(1/30)
-    print(i)
+   # print(i)
 
 p.disconnect()
+
 
     
