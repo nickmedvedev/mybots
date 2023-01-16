ef353cabfc2f4baa4ced86e9c0b89e336296511e
nickmedvedev <115434259+nickmedvedev@users.noreply.github.com>
2023-01-16 14:32:24 -0600

    36

--- simulate.py
+++ simulate.py
@@ -23,7 +23,7 @@ for i in range(1000):
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     time.sleep(1/1000)
    # print(i)
-numpy.save('./data/sensor_output.npy', backLegSensorValues)
+numpy.save('./data/backLegSensorValues.npy', backLegSensorValues)
 
 print(backLegSensorValues)
 p.disconnect()
