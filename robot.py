ccc5941790c0c6665bc719aacb53c545aba02e78
nickmedvedev <115434259+nickmedvedev@users.noreply.github.com>
2023-01-17 18:19:27 -0600

    bruh

--- robot.py
+++ robot.py
@@ -7,15 +7,11 @@ class ROBOT:
     def Sense(self):
         #print("Hey this is working right?")
         pass
-<<<<<<< HEAD
 
     def Prepare_To_Act(self):
         self.motors = {}
         for jointName in pyrosim.jointNamesToIndices:
             self.motors[jointName] = MOTOR(jointName)
-            
-=======
->>>>>>> parent of 348ff01 (Starting on Make the robot act (again))
 
     def Prepare_To_Sense(self):
         self.sensors = {}
