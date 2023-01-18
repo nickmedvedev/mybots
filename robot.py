f14b3b16b954f4e5be72838431bbab6c23a6bf84
nickmedvedev <115434259+nickmedvedev@users.noreply.github.com>
2023-01-17 18:08:13 -0600

    Starting on 82

--- robot.py
+++ robot.py
@@ -1,6 +1,7 @@
 import pybullet as p
 import pyrosim.pyrosim as pyrosim
 from sensor import SENSOR
+from motor import MOTOR
 
 class ROBOT:
     def Sense(self, i):
@@ -9,6 +10,12 @@ class ROBOT:
         for sensor in self.sensors:
             self.sensors[sensor].Get_Value(i) #currently on this
 
+    def Prepare_To_Act(self):
+        self.motors = {}
+        for jointName in pyrosim.jointNamesToIndices:
+            self.motors[jointName] = MOTOR(jointName)
+            
+
     def Prepare_To_Sense(self):
         self.sensors = {}
         for linkName in pyrosim.linkNamesToIndices:
@@ -16,7 +23,6 @@ class ROBOT:
 
 
     def __init__(self):
-        self.motors = {}
         self.robotId = p.loadURDF("body.urdf")
         pyrosim.Prepare_To_Simulate(self.robotId)
         self.Prepare_To_Sense()
