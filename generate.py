63fb08f640d80fd4f7c69228273279b2c8acfa50
nickmedvedev <115434259+nickmedvedev@users.noreply.github.com>
2023-01-23 23:44:00 -0600

    Revert "On part 14 automating search (K)"
    This reverts commit 498c1ce071811a2748a910c932a15c47c686fd28.

--- generate.py
+++ generate.py
@@ -1,6 +1,4 @@
 import pyrosim.pyrosim as pyrosim
-import random
-
 length = 1
 width = 1
 height = 1
@@ -23,23 +21,17 @@ def Generate_Body():
 
 def Generate_Brain():
     pyrosim.Start_NeuralNetwork("brain.nndf")
-
     pyrosim.Send_Sensor_Neuron(name = 0 , linkName = "Torso")
     pyrosim.Send_Sensor_Neuron(name = 1 , linkName = "BackLeg")
     pyrosim.Send_Sensor_Neuron(name = 2 , linkName = "FrontLeg")
     pyrosim.Send_Motor_Neuron( name = 3 , jointName = "Torso_BackLeg")
     pyrosim.Send_Motor_Neuron( name = 4 , jointName = "Torso_FrontLeg")
-    #nested for loop from K below:
-    for sensorNeuron in range(3):
-        for motorNeuron in range(3, 5):
-            pyrosim.Send_Synapse(sourceNeuronName = sensorNeuron, targetNeuronName = motorNeuron, weight = random.uniform(-1, 1))
-
-    #pyrosim.Send_Synapse(sourceNeuronName = 0 , targetNeuronName = 3 , weight = -1.0)
-    #pyrosim.Send_Synapse(sourceNeuronName = 1 , targetNeuronName = 3 , weight = -1.0)
-    #pyrosim.Send_Synapse(sourceNeuronName = 0 , targetNeuronName = 4 , weight = -0.5)
-    #pyrosim.Send_Synapse(sourceNeuronName = 2 , targetNeuronName = 4 , weight = 0.5)
-    #pyrosim.Send_Synapse(sourceNeuronName = 1 , targetNeuronName = 4 , weight = -1.0)
-    #pyrosim.Send_Synapse(sourceNeuronName = 2, targetNeuronName = 3, weight = -0.5)
+    pyrosim.Send_Synapse(sourceNeuronName = 0 , targetNeuronName = 3 , weight = -1.0)
+    pyrosim.Send_Synapse(sourceNeuronName = 1 , targetNeuronName = 3 , weight = -1.0)
+    pyrosim.Send_Synapse(sourceNeuronName = 0 , targetNeuronName = 4 , weight = -0.5)
+    pyrosim.Send_Synapse(sourceNeuronName = 2 , targetNeuronName = 4 , weight = 0.5)
+    pyrosim.Send_Synapse(sourceNeuronName = 1 , targetNeuronName = 4 , weight = -1.0)
+    pyrosim.Send_Synapse(sourceNeuronName = 2, targetNeuronName = 3, weight = -0.5)
     pyrosim.End()
 
 
