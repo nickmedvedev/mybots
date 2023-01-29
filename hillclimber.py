f38ead5e22028129462a6cc735d6e0d02f282074
nickmedvedev <115434259+nickmedvedev@users.noreply.github.com>
2023-01-29 14:27:53 -0600

    Starting on 34 (L)

--- hillclimber.py
+++ hillclimber.py
@@ -3,4 +3,7 @@ from solution import SOLUTION
 class HILL_CLIMBER:
     def __init__(self):
         self.parent = SOLUTION()
-        pass
+        
+
+    def Evolve(self):
+        self.parent.Evaluate()
