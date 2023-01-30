5bf1685512cea1891fd80795a0271e4ae370af17
nickmedvedev <115434259+nickmedvedev@users.noreply.github.com>
2023-01-30 15:10:41 -0600

    Step 9 M

---
+++ parallelHillClimber.py
@@ -0,0 +1,38 @@
+from solution import SOLUTION
+import constants as c
+import copy
+
+class HILL_CLIMBER:
+    def __init__(self):
+        self.parent = SOLUTION()
+    
+    def Show_Best(self):
+        self.parent.Evaluate("GUI")
+
+    def Spawn(self):
+        self.child = copy.deepcopy(self.parent)
+
+    def Mutate(self):
+        self.child.Mutate()
+
+    def Select(self):
+        if self.parent.fitness > self.child.fitness:
+            self.parent = self.child
+        #print("this is the parent fitness" + str(self.parent.fitness))
+        #print("this is the child fitness" + str(self.child.fitness))
+    
+    def Print(self):
+        print("This is the fitness of the parent: " + str(self.parent.fitness) + " This is the fitness of the child: " + str(self.child.fitness))
+    
+    def Evolve_For_One_Generation(self):
+        self.Spawn()
+        self.Mutate()
+        self.child.Evaluate("DIRECT")
+        self.Print()
+        self.Select()
+
+    def Evolve(self):
+        self.parent.Evaluate("GUI")
+        for currentGeneration in range(c.numberOfGenerations):
+            self.Evolve_For_One_Generation()
+                
